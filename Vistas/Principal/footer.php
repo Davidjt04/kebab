767dbf4c29ff3034847c4abb1bb88597fef8b7cc
Davidjt04 <dajantor@gmail.com>
2024-11-05 11:54:21 +0100

    tercer commit

--- Vistas/Principal/footer.php
+++ Vistas/Principal/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer">
     <div class="container">
-        <div class="text-center">
-            <small>Copyright © Departamento de Informática 2019</small>
-        </div>
+        <div id = "logo"></div>
+        <div id = "direccion"></div>
+        <div id = "informacion"></div>
     </div>
 </footer>
